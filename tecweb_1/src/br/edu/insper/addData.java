9d48786c24ff55e7a925486ab4282dfe1e1c4cad
tluigimm <tluigimm@gmail.com>
2020-09-04 09:26:57 -0300

    html & servlet

---
+++ tecweb_1/src/br/edu/insper/addData.java
@@ -0,0 +1,56 @@
+package br.edu.insper;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class addData
+ */
+@WebServlet("/addData")
+public class addData extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public addData() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+		String nome = request.getParameter("nome");
+		String email = request.getParameter("email");
+		String curso = request.getParameter("curso");
+		
+		PrintWriter out = response.getWriter();
+		
+        out.println("<html>");
+        out.println("nome: " + nome + "<br/>");      
+        out.println("email: " + email + "<br/>");    
+        out.println("curso: " + curso + "<br/>");    
+        out.println("</html>");
+    }
+
+}
